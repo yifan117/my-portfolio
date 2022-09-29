7b27f1d8a6f6841f0b58dc16fc7b1690c602b17a
yifan117 <yifan.qiu17@gmail.com>
2022-09-29 17:32:26 +0930

    test fr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
-<div class="body">
-    <p>Ava has nice boobs hehehe</p>
+<div class="page">
+    <p>under construction</p>
 </div>
 
 
 <style>
-    .body {
+    .page {
         display: flex;
         align-items: center;
         justify-content: center;
+        height: 100vh;
     }
 
     p {
