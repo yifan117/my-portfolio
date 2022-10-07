21fb423b26d4dc707517687a229907a2163d9cb4
yifan117 <yifan.qiu17@gmail.com>
2022-10-07 18:16:15 +1030

    added content to hire page

--- src/routes/hire/+page.svelte
+++ src/routes/hire/+page.svelte
@@ -6,6 +6,15 @@
 
 <body>
     <Nav/>
+
+    <div class="wrapper">
+        <div class="text-container">
+            <h1>What Am I Looking For?</h1>
+            <p>I'm looking for opportunities as either a full-stack, front-end, or back-end software engineer. Since I am only in my first year of my Computer Science degree, I'm primarily focusing on internships and part-time jobs, but would love the opportunity for permanent offers once I graduate!</p>
+            <p>To contact me about offers or if you have any questions, please click <a href="../contact-me">here</a></p>
+        </div>
+    </div>
+        
 </body>
 
 <style>
@@ -18,5 +27,35 @@
 
     body {
         background: #1D1D1D;
+        position: absolute;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        min-height: 100vh;
+    }
+
+    h1 {
+        text-transform: uppercase;
+    }
+
+    .text-container {
+        width: 80%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1.3em;
+    }
+
+    .wrapper {
+        display: flex;
+        align-self: stretch;
+        align-items: center;
+        justify-content: center;
+        height: 70%;
+    }
+
+    a {
+        text-decoration: none;
+        color: #2DA2E4;
     }
 </style>
