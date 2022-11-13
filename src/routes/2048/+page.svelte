d0484de5c76dbe6af68226b98b15439f500748b8
yifan117 <yifan.qiu17@gmail.com>
2022-11-13 12:49:29 +1030

    need to add animation

--- src/routes/2048/+page.svelte
+++ src/routes/2048/+page.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
-	import { onMount } from "svelte";
 
     let gameArr = [
-        [2, 2, 2, 2],
-        [2, 0, 0, 0],
-        [2, 2, 2, 2],
-        [2, 0, 0, 0]
+        [0, 128, 0, 0],
+        [0, 128, 2, 0],
+        [2, 0, 256, 0],
+        [512, 0, 0, 0]
     ]
 
-    // iterate through array, find non zeros
-    // move all to start
-    // merge 
-
-    enum Code {
-        ArrowUp = "ArrowUp",
-        ArrowDown = "ArrowDown",
-        ArrowLeft = "ArrowLeft",
-        ArrowRight = "ArrowRight"
-    }
-
     let code;
+    let score = 0;
+    let scoreUpdate = 0;
+
+    function updateScore() {
+        score = 2*scoreUpdate;
+    }
 
     function handleKeydown(event: any) {
         code = event.key;
@@ -42,6 +36,11 @@
 
             case "ArrowLeft": {
                 keyLeft();
+                break;
+            }
+
+            case "Enter": {
+                resetGame();
                 break;
             }
 
@@ -94,6 +93,8 @@
 
         randomFill();
         randomFill();
+
+        score = 0;
     }
 
     function move_up() {
@@ -123,6 +124,7 @@
 
             for (let i = 0; i < 3; i++) {
                 if (gameArr[i][a] === gameArr[i+1][a]) {
+                    scoreUpdate += gameArr[a][i];
                     gameArr[i][a] = gameArr[i][a] * 2;
                     gameArr[i+1][a] = 0; 
                 }
@@ -160,6 +162,7 @@
         for (let a = 0; a < 4; a++) {
             for (let i = 3; i > 0; i--) {
                 if (gameArr[i][a] === gameArr[i-1][a]) {
+                    scoreUpdate += gameArr[a][i];
                     gameArr[i][a] = gameArr[i][a] * 2;
                     gameArr[i-1][a] = 0; 
                 }
@@ -197,6 +200,7 @@
         for (let a = 0; a < 4; a++) {
             for (let i = 3; i > 0; i--) {
                 if (gameArr[a][i] === gameArr[a][i-1]) {
+                    scoreUpdate += gameArr[a][i];
                     gameArr[a][i] = gameArr[a][i] * 2;
                     gameArr[a][i-1] = 0; 
                 }
@@ -231,6 +235,7 @@
         for (let a = 0; a < 4; a++) {
             for (let i = 0; i < 3; i++) {
                 if (gameArr[a][i] === gameArr[a][i+1]) {
+                    scoreUpdate += gameArr[a][i];
                     gameArr[a][i] = gameArr[a][i] * 2;
                     gameArr[a][i+1] = 0; 
                 }
@@ -245,9 +250,9 @@
         move_left();
         merge_left();
         move_left();
-        check_loss();
         randomFill();
-
+        check_loss();
+        updateScore();
     }
 
     function keyRight() {
@@ -255,8 +260,9 @@
         move_right();
         merge_right();
         move_right();
-        check_loss();
         randomFill();
+        check_loss();
+        updateScore();
     }
 
     function keyUp() {
@@ -264,8 +270,9 @@
         move_up();
         merge_up();
         move_up();
-        check_loss();
         randomFill();
+        check_loss();
+        updateScore();
     }
 
     function keyDown() {
@@ -273,30 +280,99 @@
         move_down();
         merge_down();
         move_down();
-        check_loss();
         randomFill();
+        check_loss();
+        updateScore();
     }
 
 </script>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Prompt">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,800&display=swap">
 <svelte:window on:keydown={handleKeydown}/>
 
 <div class="page">
-    <div class="arrayContainer">
-        <div class="array">
-            {#each Array(4) as ab, a}
-                {#each Array(4) as _, i}
-                    <div class="element">
-                        {gameArr[i][a]}
-                    </div>
-                {/each}
-            {/each}
-        </div>
-    </div>
+    <div class="gameContainer">
 
-    <div class="newGame">
-        <button on:click={resetGame}>New Game</button>
+        <div class="headerContainer">
+            <div class="headerTextContainer">
+                <div class="gameName">
+                    2048
+                </div>
+
+                <div class="subText">
+                    Join the tiles, get to <b>2048!</b>
+                </div>
+            </div>
+
+            <div class="functionsContainer">
+                <div class="scoresContainer">
+                    <div class="score">
+                        <p class="word">SCORE</p>
+                        <p>{score}</p>
+                    </div>
+
+                    <div class="score">
+                        <p class="word">BEST</p>
+                        <p>{score}</p>
+                    </div>
+                </div>
+
+                <div class="newGame">
+                    <button on:click={resetGame}>New Game</button>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="arrayContainer">
+            <div class="array">
+                {#each Array(4) as ab, a}
+                    {#each Array(4) as _, i}
+                        {#if gameArr[a][i] == 2}
+                            <div class="element" style="background-color: var(--two-bg); color: var(--two-font);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 4}
+                            <div class="element" style="background-color: var(--four-bg); color: var(--four-font);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 8}
+                            <div class="element" style="background-color: var(--eight-bg); color: var(--eight-font);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 16}
+                            <div class="element" style="background-color: var(--sixteen-bg); color: var(--sixteen-font);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 32}
+                            <div class="element" style="background-color: var(--thirtytwo-bg); color: var(--thirtytwo-font);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 64}
+                            <div class="element" style="background-color: var(--sixtyfour-bg); color: var(--sixtyfour-font);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 128}
+                            <div class="element" style="background-color: var(--onehundredtwentyeight-bg); color: var(--onehundredtwentyeight-font); box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.5), inset 0 0 0 1px rgba(255, 255, 255, 0.190476);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 256}
+                            <div class="element" style="background-color: var(--twohundredfiftysix-bg); color: var(--twohundredfiftysix-font); box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.8), inset 0 0 0 1px rgba(255, 255, 255, 0.190476);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else if gameArr[a][i] == 512}
+                            <div class="element" style="background-color: var(--fivehundredtwelve-bg); color: var(--fivehundredtwelve-font); box-shadow: 0 0 30px 10px rgba(243, 220, 80, 1), inset 0 0 0 1px rgba(255, 255, 255, 0.190476);">
+                                {gameArr[a][i]}
+                            </div>
+                        {:else}
+                            <div class="element" style="background-color: var(--empty-bg); color: var(--empty-font);">
+                                {gameArr[a][i]}
+                            </div>
+                        {/if}
+                    {/each}
+                {/each}
+            </div>
+        </div>
     </div>
 </div>
 
@@ -305,16 +381,38 @@
 <style>
 
 * {
-    font-family: 'Prompt';
+    /* font-family: 'Prompt'; */
+    font-family: "Open Sans", "Helvetica Neue", Arial, sans-serif;
+    animation: appear 200ms ease 100ms;
+    animation-fill-mode: backwards;
 }
 
 :root {
-    --font-color: #756f64;
-    --empty-space: #948a80;
+    --empty-font: rgba(249, 246, 242, 0);
+    --two-font: #776e65;
+    --four-font: #776e65;
+    --eight-font: #f9f6f2;
+    --sixteen-font: #f9f6f2;
+    --thirtytwo-font: #f9f6f2;
+    --sixtyfour-font: #f9f6f2;
+    --onehundredtwentyeight-font: #f9f6f2;
+    --twohundredfiftysix-font: #f9f6f2;
+    --fivehundredtwelve-font: #f9f6f2;
+
+    --empty-bg: rgba(238, 228, 218, 0.35);
+    --two-bg: #eee4da;
+    --four-bg: #eee1c9;
+    --eight-bg: #f3b27a;
+    --sixteen-bg: #f69664;
+    --thirtytwo-bg: #f77c5f;
+    --sixtyfour-bg: #f75f3b;
+    --onehundredtwentyeight-bg: #edcc62;
+    --twohundredfiftysix-bg: #edcc62;
+    --fivehundredtwelve-bg: #edcc62;
+
     --box-color: #bbada0;
-    --two-color: #eddfc5;
-    --four-color: #eddfc5;
-    --eight-color: #f0af77;
+    --text-white: #f9f6f2;
+    --game-brown: #8f7a66;
 }
 
 .page {
@@ -324,11 +422,9 @@
     left: 0;
     right: 0;
     display: flex;
-    background: #1d1d1d;
+    background: #faf8ef;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
-    gap: 30px;
 }
 
 .arrayContainer {
@@ -348,9 +444,9 @@
     display: grid;
     height: 470px;
     width: 470px;
-    grid-template-columns: 100px 100px 100px 100px;
-    grid-template-rows: 80px auto 80px; 
-    column-gap: 10px;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr); 
+    column-gap: 15px;
     row-gap: 15px;
 }
 
@@ -358,6 +454,97 @@
     display: flex;
     height: 100%;
     width: 100%;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--empty-space);
+    font-size: 3em;
+    font-weight: 800;
+    transition: 100ms ease-in-out;
+    transition-property: transform;
+    border-radius: 5px;
 }
 
+button {
+    margin: none;
+    border: none;
+    background: var(--game-brown);
+    padding: 8px 14px 8px 14px;
+    color: var(--text-white);
+    font-size: 1em;
+    border-radius: 5px;
+    font-weight: 800;
+    cursor: pointer;
+}
+
+.gameName {
+    color: var(--game-brown);
+    font-size: 4em;
+    font-weight: 800;
+}
+
+.subText {
+    color: var(--game-brown);
+    font-size: 1.2em;
+}
+
+.headerContainer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.gameContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+
+.functionsContainer {
+    display: flex;
+    flex-direction: column;
+    height: 50%;
+    width: 50%;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+}
+
+.scoresContainer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    gap: 10px;
+}
+
+.score {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 80%;
+    height: 50%;
+    color: var(--text-white);
+    background: var(--box-color);
+    flex-direction: column;
+    border-radius: 5px;
+    padding: 5px 0px 5px 0px;
+}
+
+p {
+    padding: none;
+    margin: 0px;
+    font-weight: 800;
+    font-size: 1.5em;
+}
+
+.word {
+    opacity: 70%;
+    font-size: 0.8em;
+}
+
+.newGText {
+    display: flex;
+    color: var(--game-brown);
+}
 </style>
