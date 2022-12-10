815e4908887902b529c24c1ee2f3b0ee27a25a84
yifan117 <yifan.qiu17@gmail.com>
2022-12-10 14:47:25 +1030

    redirected to new port

--- src/routes/lib/nav-bar.svelte
+++ src/routes/lib/nav-bar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
         let headerContents = [
         { content: "Home", link: "../"},
-        { content: "Portfolio", link: "../portfolio"},
+        { content: "Portfolio", link: "../portfolio_revised"},
         { content: "Hire", link: "../hire"},
         { content: "Contact", link: "../contact-me"}
     ];
