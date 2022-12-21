314c98880be8bd2e825b15fa63c1afa05d2965e3
yifan117 <yifan.qiu17@gmail.com>
2022-12-21 20:59:51 +1030

    resized footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -9,52 +9,36 @@ import Github from "svelte-material-icons/Github.svelte";
 
 <Border padding="2px 0px 0px 0px" border_radius="0px">
     <div class="container">
-        <div class="top">
-            <div class="logo">
-                [yifan]
+        <a class="icon_wrapper" href="https://www.github.com/yifan117" target="blank">
+            <div class="text">
+                GitHub
             </div>
-        </div>
 
-        <div class="bottom">
-            <a class="icon_wrapper" href="https://www.github.com/yifan117" target="blank">
-                <div class="text">
-                    GitHub
-                </div>
+            <div class="icon">
+                <Github color="#D8D8E7"/>
+            </div>
+        </a>
+        <a class="icon_wrapper" href="https://www.linkedin.com/in/yifan117" target="blank">
+            <div class="text">
+                LinkedIn
+            </div>
 
-                <div class="icon">
-                    <Github color="#D8D8E7"/>
-                </div>
-            </a>
-            <a class="icon_wrapper" href="https://www.linkedin.com/in/yifan117" target="blank">
-                <div class="text">
-                    LinkedIn
-                </div>
+            <div class="icon">
+                <Linkedin color="#D8D8E7"/>
+            </div>
+        </a>
+        <a class="icon_wrapper" href="https://drive.google.com/file/d/1kdfccKUIHdXxzvFzR24ye90R7cVwHjKc/view?usp=sharing" target="blank">
+            <div class="text">
+                Resume
+            </div>
 
-                <div class="icon">
-                    <Linkedin color="#D8D8E7"/>
-                </div>
-            </a>
-            <a class="icon_wrapper" href="https://drive.google.com/file/d/1kdfccKUIHdXxzvFzR24ye90R7cVwHjKc/view?usp=sharing" target="blank">
-                <div class="text">
-                    Resume/CV
-                </div>
-
-                <div class="icon">
-                    <Resume color="#D8D8E7"/>
-                </div>
-            </a>
-        </div>
+            <div class="icon">
+                <Resume color="#D8D8E7"/>
+            </div>
+        </a>
     </div>
 </Border>
 <style lang='stylus'>
-.logo
-    order 0
-    display flex
-    font-size 2em
-    font-weight 700
-    align-items center
-    justify-content center
-    color #D8D8E7
 
 .container
     z-index 2
@@ -62,33 +46,23 @@ import Github from "svelte-material-icons/Github.svelte";
     width 100%
     display flex
     align-items center
-    justify-content center
-    gap 100px
+    justify-content space-around
     padding 8px
     width 100%
-
-.bottom
-    display flex
-    align-items center
-    justify-content space-between
-    gap 40px
 
 .text
     text-align center
     font-weight 600
-    font-size 16px
+    font-size 1rem
+    text-size-adjust auto
 
 .icon
     display flex
     align-items center
     justify-content center
     padding 2px
-    font-size 50px
-
-.top
-    display flex
-    align-items center
-    justify-content center
+    font-size 2rem
+    text-size-adjust auto
 
 a
     text-decoration none
@@ -101,9 +75,8 @@ a
     align-items center
     justify-content center
     cursor pointer
-    width clamp(40px, 100px, 120px)
     aspect-ratio 1
-    padding 2px
+    padding 4px
     border-radius 4px
 
     &:hover
