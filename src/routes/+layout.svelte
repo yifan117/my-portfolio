140868ef85efa6f9a02415ff5d320119ab41ff78
yifan117 <yifan.qiu17@gmail.com>
2022-12-25 01:30:41 +1030

    added custom scroll to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,27 @@
 
     <slot/>
     
-<style>
+<style lang='stylus' global>
 
-:global(body) {
-    margin: 0px;
-}
+body
+    margin 0px
+
+/* width */
+::-webkit-scrollbar
+    height 6px
+    width 8px
+
+/* Track */
+::-webkit-scrollbar-track
+    box-shadow inset 0 0 0.5px grey 
+    border-radius 10px
+
+/* Handle */
+::-webkit-scrollbar-thumb
+    background rgba(255, 255, 255, 0.15)
+    border-radius 10px
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover
+    background rgba(255, 255, 255, 0.05)
 </style>
