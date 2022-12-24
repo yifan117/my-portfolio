911bfc1b94c735a6afad2c8de6d1ef2cdb56c259
yifan117 <yifan.qiu17@gmail.com>
2022-12-25 01:38:47 +1030

    converted contact page to stylus

--- src/routes/contact-me/+page.svelte
+++ src/routes/contact-me/+page.svelte
@@ -55,144 +55,128 @@
     <Footer/>
 </body>
 
-<style>
+<style lang='stylus'>
 
-    .wrapper {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    }
+.wrapper
+    display flex
+    align-items center
+    justify-content center
+    flex-direction column
 
-    body {
-        display: flex;
-        width: 100vw;
-        margin: 0px;
-        height: 100vh;
-        flex-direction: column;
-        align-items: center;
-        background: #1D1D1D;
-        overflow-x: hidden;
-    }
+body
+    display flex
+    width 100vw
+    margin 0px
+    height 100vh
+    flex-direction column
+    align-items center
+    background #1D1D1D
+    overflow-x hidden
 
-    .container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: #1D1D1D;
-        gap: 50px;
-}
+.container
+    display flex
+    align-items center
+    justify-content center
+    background #1D1D1D
+    gap 50px
 
-    * {
-        color: #D8D8E7;
-        font-family: 'Prompt';
-        box-sizing: border-box;
-        text-size-adjust: auto;
-    }        
-    
-    h3 {
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        -webkit-text-stroke: 1px #D8D8E7;
-        text-align: center;
-    }
-    
-    span {
-        color: #D8D8E7;
-    }
-    
-    .card {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 80px;
-        flex-direction: column;
-        width: 40%;
-        height: 70%;
-        background: rgba(255, 255, 255, 0.15);
-        border-radius: 16px;
-        gap: 8px;
-    }
+*
+    color #D8D8E7
+    font-family 'Prompt'
+    box-sizing border-box
+    text-size-adjust auto        
 
-    .inputBox {
-        position: relative;
-        min-width: 250px;
-        margin-bottom: 8px;
-        color: #D8D8E7;
-    }
-    
-    .inputBox input, textarea {
-        width: 100%;
-        padding: 10px;
-        outline: none;
-        border: none;
-        color: #D8D8E7;
-        font-size: 1em;
-        background: transparent;
-        border-left: 2px solid #1D1D1D;
-        border-bottom: 2px solid #1D1D1D;
-        transition: 0.1s;
-    }
-    
-    .inputBox input:focus, textarea:focus {
-        border: 2px solid #1D1D1D;
-        transition: 1s;
-    }
-    
-    .inputBox input:valid~span,
-    .inputBox input:focus~span {
-        transform: translateX(160px) translateY(-18px);
-        font-size: 0.8em;
-        padding: 5px 10px;
-        background: #1D1D1D;
-        letter-spacing: 0.2em;
-        color: #D8D8E7;
-        border: 2px;
-    }
-    
-    .inputBox input:valid,
-    .inputBox input:focus {
-        border: 2px solid #1D1D1D;
-    }
-    
-    input[type=submit] {
-        height: 45px;
-        width: 100px;
-        border-radius: 5px;
-        border: 2px solid #000;
-        cursor: pointer;
-        background-color: transparent;
-        transition: 0.5s;
-        text-transform: uppercase;
-        font-size: 10px;
-        letter-spacing: 2px;
-        font-weight: 700
-    }
-    
-    input[type=submit]:hover {
-        background-color: #000;
-        color: D8D8E7;
-    }
-    
-    .inputBox span {
-        margin-top: 5px;
-        position: absolute;
-        left: 0;
-        transform: translateY(-4px);
-        margin-left: 10px;
-        padding: 10px;
-        pointer-events: none;
-        font-size: 12px;
-        color: #D8D8E7;
-        text-transform: uppercase;
-        transition: 0.5s;
-        letter-spacing: 3px;
-    }
+h3
+    text-transform uppercase
+    letter-spacing 2px
+    -webkit-text-stroke 1px #D8D8E7
+    text-align center
 
-    @media (max-width: 1000px) {
+span
+    color #D8D8E7
 
-    .container {
-        flex-direction: column;
-    }
-}
+.card
+    display flex
+    justify-content center
+    align-items center
+    padding 80px
+    flex-direction column
+    width 40%
+    height 70%
+    background rgba(255, 255, 255, 0.15)
+    border-radius 16px
+    gap 8px
+
+.inputBox
+    position relative
+    min-width 250px
+    margin-bottom 8px
+    color #D8D8E7
+
+.inputBox input, textarea
+    width 100%
+    padding 10px
+    outline none
+    border none
+    color #D8D8E7
+    font-size 1em
+    background transparent
+    border-left 2px solid #1D1D1D
+    border-bottom 2px solid #1D1D1D
+    transition 0.1s
+
+.inputBox input:focus, textarea:focus
+    border 2px solid #1D1D1D
+    transition 1s
+
+.inputBox input:valid~span,
+.inputBox input:focus~span
+    transform translateX(160px) translateY(-18px)
+    font-size 0.8em
+    padding 5px 10px
+    background #1D1D1D
+    letter-spacing 0.2em
+    color #D8D8E7
+    border 2px
+
+.inputBox input:valid,
+.inputBox input:focus
+    border 2px solid #1D1D1D
+
+input[type=submit]
+    height 45px
+    width 100px
+    border-radius 5px
+    border 2px solid #000
+    cursor pointer
+    background-color transparent
+    transition 0.5s
+    text-transform uppercase
+    font-size 10px
+    letter-spacing 2px
+    font-weight 700
+
+input[type=submit]:hover
+    background-color #000
+    color D8D8E7
+
+.inputBox span
+    margin-top 5px
+    position absolute
+    left 0
+    transform translateY(-4px)
+    margin-left 10px
+    padding 10px
+    pointer-events none
+    font-size 12px
+    color #D8D8E7
+    text-transform uppercase
+    transition 0.5s
+    letter-spacing 3px
+
+@media (max-width 1000px)
+
+    .container
+        flex-direction column
+
 </style>
